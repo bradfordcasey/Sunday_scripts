8aeb82ab2bfe577115e9ece3f455eef8e8d33bd3
bradfordcasey <bradcasey@gmail.com>
2015-04-19 14:51:01 -0500

    Added summary of pop by cont by year, prior to 1990

--- Sunday_pm_gapminder.R
+++ Sunday_pm_gapminder.R
@@ -4,8 +4,8 @@
 # Apr 19 2015
 
 #install dplyr package with dependencies
-#installs all dependencies needed for the given package
-install.packages("dplyr", dependencies = TRUE)
+#installs all dependencies needed for the given package, hash out after running once
+#install.packages("dplyr", dependencies = TRUE)
 
 #loads library for dplyr into working environment
 library("dplyr")
@@ -15,5 +15,22 @@ gap.in <- read.table("output/combined_gapMinder.tsv",
                      sep="\t",
                      header = TRUE)
 View(gap.in)
+#gap.in %>%
+#  filter(country == "China") %>%
+# select(country, year, pop) %>%
+#  group_by(country) %>%
+#  summarize(min = min(pop))
+
+#Challenge
+#calculate mean population per continent per year for years prior to 1990
 
 
+gap.out <- gap.in %>%
+  filter(year < 1990) %>%
+  select(continent, year, pop) %>%
+  group_by(continent,year) %>%
+  summarize(mean = mean(pop))
+gap.out <- summarize
+view(gap.out)
+  
+  
